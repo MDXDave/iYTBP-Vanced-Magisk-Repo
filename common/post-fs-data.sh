016bca8246bd1a8176a497348b5e2a9b0951f2d7
ZaneZam <cyxman@yahoo.com>
2018-01-14 18:12:58 +0100

    Added automatic playstore detach/attach functionality
    Description:
    
    Use following files by putting them into /data/ to take effect
    
    File "iytbp-module-detach-disabled"
    Use this if u want to detach YT app from playstore on every boot (like the magisk installer does by default)
    
    File "iytbp-module-detach-enabled"
    Use this if u want to attach previosly detached YT app to playstore again. This file has priority over the detach flag file above and gets
    deleted after first execution of the attach function (because it's just needed to execute once and not needed on every boot)
    
    If both of the flag files are deleted from /data/ the boot script is disabled and no detach or attach function will be executed at bootime.
    
    The flag files can be found in modules directory which usually is /magisk/iYTBP-Vanced-Magisk-Repo

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,7 +1,38 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
+
 MODDIR=${0%/*}
 
-# This script will be executed in post-fs-data mode
-# More info in the main Magisk thread
+# Detach/Attach Apps from playstore by hinxnz modified for the iYTBP Vanced project
+
+PLAY_DB_DIR=/data/data/com.android.vending/databases
+DETACHENABLED=/data/iytbp-module-detach-enabled
+DETACHDISABLED=/data/iytbp-module-detach-disabled
+
+if [ ! -e $DETACHENABLED ] && [ ! -e $DETACHDISABLED ]; then
+    exit 0
+fi
+
+if [ -e $DETACHDISABLED ]; then
+    (while [ 1 ]; do
+    if [ `getprop sys.boot_completed` = 1 ]; then sleep 60
+	pm enable 'com.android.vending/com.google.android.finsky.dailyhygiene.DailyHygiene'$'DailyHygieneService\'
+	pm enable 'com.android.vending/com.google.android.finsky.hygiene.DailyHygiene'$'DailyHygieneService\'
+	am startservice 'com.android.vending/com.google.android.finsky.dailyhygiene.DailyHygiene'$'DailyHygieneService\'
+	am startservice 'com.android.vending/com.google.android.finsky.hygiene.DailyHygiene'$'DailyHygieneService\'
+	rm -f $DETACHENABLED
+	rm -f $DETACHDISABLED
+	exit; fi
+    done &)
+fi
+
+if [ -e $DETACHENABLED ]; then
+    (while [ 1 ]; do
+	if [ `getprop sys.boot_completed` = 1 ]; then sleep 60
+	pm disable 'com.android.vending/com.google.android.finsky.hygiene.DailyHygiene$DailyHygieneService'
+	am force-stop com.android.vending
+	cd $MODDIR
+	./sqlite $PLAY_DB_DIR/library.db "DELETE from ownership where doc_id = 'com.google.android.youtube'";
+	./sqlite $PLAY_DB_DIR/localappstate.db "DELETE from appstate where package_name = 'com.google.android.youtube'";
+	exit; fi
+    done &)
+fi
